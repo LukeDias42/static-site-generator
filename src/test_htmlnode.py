1eee6a455cc2f7b63b4e15a2b36299c2eb77353e
Luke Dias <lhollowwizard@gmail.com>
2024-10-29 20:45:18 -0300

    test(htmlnode): add tests for html node

---
+++ src/test_htmlnode.py
@@ -0,0 +1,42 @@
+import unittest
+
+from htmlnode import HTMLNode
+from textnode import TextNode, TextType
+
+
+class TestHTMLNode(unittest.TestCase):
+    def test_props_to_html_nothing(self):
+        node = HTMLNode()
+        self.assertEqual(node.props_to_html(), "")
+
+    def test_props_to_html_one_prop(self):
+        node = HTMLNode(
+            props={
+                "href": "https://www.google.com",
+            }
+        )
+        self.assertEqual(node.props_to_html(),
+                         ' href="https://www.google.com"')
+
+    def test_props_to_html_more_props(self):
+        node = HTMLNode(
+            props={
+                "href": "https://www.google.com",
+                "target": "_blank",
+            }
+        )
+        self.assertEqual(
+            node.props_to_html(), ' href="https://www.google.com" target="_blank"'
+        )
+
+    def test_repr(self):
+        node = HTMLNode("a", "this is a link", None, {
+                        "href": "https://www.google.com"})
+        self.assertEqual(
+            f"{node}",
+            "HTMLNode(a, this is a link, None, {'href': 'https://www.google.com'})",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()

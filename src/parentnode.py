0fc6e3cfe62b21bfd90d111080f4ba607dcfcb05
Luke Dias <lhollowwizard@gmail.com>
2024-10-30 22:00:45 -0300

    feat(parentnode): add parent node
    Parent Node is responsible for turning a parent node (which can have any
    number of children and parent nodes as children) into html.

---
+++ src/parentnode.py
@@ -0,0 +1,17 @@
+from htmlnode import HTMLNode
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Parent Node needs a tag to be parsed.")
+        if self.children is None:
+            raise ValueError("Parent Node needs children to be parsed.")
+        result = f"<{self.tag}{self.props_to_html()}>"
+        for child in self.children:
+            result += f"{child.to_html()}"
+        result += f"</{self.tag}>"
+        return result

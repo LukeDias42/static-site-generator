a865b0625c0b3dff2b061b42b0f9bbbe59c117fb
Luke Dias <lhollowwizard@gmail.com>
2024-10-31 20:58:32 -0300

    feat(inline_markdown): add split node by image and link
    These functions are responsible for breaking a list of nodes into the
    correct image and link node types.

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -30,6 +30,42 @@ def extract_markdown_images(text):
 
 
 def extract_markdown_links(text):
-    image_regex = r"(?<!!)\[(.*?)\]\((.*?)\)"
-    result = re.findall(image_regex, text)
+    link_regex = r"(?<!!)\[(.*?)\]\((.*?)\)"
+    result = re.findall(link_regex, text)
     return result
+
+
+def split_nodes_image(old_nodes):
+    image_regex = r"\!\[.*?\]\(.*?\)"
+    new_nodes = []
+    for old_node in old_nodes:
+        sections = re.split(image_regex, old_node.text)
+        split_nodes = []
+        images_data = extract_markdown_images(old_node.text)
+        for i in range(len(sections)):
+            if sections[i] != "":
+                split_nodes.append(TextNode(sections[i], TextType.TEXT))
+            if i < len(sections) - 1:
+                split_nodes.append(
+                    TextNode(images_data[i][0], TextType.IMAGE, images_data[i][1])
+                )
+        new_nodes.extend(split_nodes)
+    return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    link_regex = r"(?<!!)\[.*?\]\(.*?\)"
+    new_nodes = []
+    for old_node in old_nodes:
+        sections = re.split(link_regex, old_node.text)
+        split_nodes = []
+        link_data = extract_markdown_links(old_node.text)
+        for i in range(len(sections)):
+            if sections[i] != "":
+                split_nodes.append(TextNode(sections[i], TextType.TEXT))
+            if i < len(sections) - 1:
+                split_nodes.append(
+                    TextNode(link_data[i][0], TextType.LINK, link_data[i][1])
+                )
+        new_nodes.extend(split_nodes)
+    return new_nodes

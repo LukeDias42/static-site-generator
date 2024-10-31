51634afbee8684cecd5da5b46895157220c36978
Luke Dias <lhollowwizard@gmail.com>
2024-10-30 22:01:38 -0300

    feat(textnode): add function to parse text node into leaf node
    Since text types are basic types, they can only be turned into leaf
    nodes

--- src/textnode.py
+++ src/textnode.py
@@ -1,8 +1,9 @@
 from enum import Enum
+from leafnode import LeafNode
 
 
 class TextType(Enum):
-    NORMAL = "normal"
+    TEXT = "normal"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
@@ -25,3 +26,19 @@ class TextNode:
 
     def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+
+def text_node_to_html_node(text_node):
+    if text_node.text_type == TextType.TEXT.value:
+        return LeafNode(text_node.text)
+    if text_node.text_type == TextType.BOLD.value:
+        return LeafNode(text_node.text, "b")
+    if text_node.text_type == TextType.ITALIC.value:
+        return LeafNode(text_node.text, "i")
+    if text_node.text_type == TextType.CODE.value:
+        return LeafNode(text_node.text, "code")
+    if text_node.text_type == TextType.LINK.value:
+        return LeafNode(text_node.text, "a", {"href": text_node.url})
+    if text_node.text_type == TextType.IMAGE.value:
+        return LeafNode("", "img", {"src": text_node.url, "alt": text_node.text})
+    raise ValueError("Unkown Text Type")

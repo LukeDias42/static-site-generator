8ba4a0a004ecd443a25fa25a83a00a2bff5d9e29
Luke Dias <lhollowwizard@gmail.com>
2024-11-03 03:12:30 -0300

    feat(block_markdown): add BlockType enum
    This enum has the possible types of a block

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,6 +1,15 @@
 from textnode import TextNode, TextType
+from enum import Enum
+import re
 
 
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
 def markdown_to_blocks(markdown: str) -> list[str]:
     clean_markdown = "\n".join([line.strip()
                                for line in markdown.splitlines()])

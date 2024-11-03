733ddd4a92c3e2df4731d70b19054f9283800c4e
Luke Dias <lhollowwizard@gmail.com>
2024-11-03 03:15:51 -0300

    feat(block_markdown): add initial block to block type
    This also adds the validation for the heading type, which allows any
    string that starts with 1 to 6 of the character '#'

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -15,3 +15,8 @@ def markdown_to_blocks(markdown: str) -> list[str]:
     return [block.strip() for block in markdown.split("\n\n") if len(block) >= 1]
 
 
+heading_regex = r"^#{1,6}\s"
+def block_to_block_type(block: str) -> BlockType:
+    if re.match(heading_regex, block):
+        return BlockType.HEADING
+    return BlockType.PARAGRAPH

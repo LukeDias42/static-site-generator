91c02de902ad523c23ccc7394fc19f4c69e25922
Luke Dias <lhollowwizard@gmail.com>
2024-11-05 21:50:21 -0300

    feat(block_markdown): better regexes and regex for quote
    The new regexes are able to group the "value" part of the different
    blocks, which allow for more simple logic for getting the data.

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,6 +1,10 @@
 from enum import Enum
 import re
 
+heading_regex = r"^(#{1,6}) ([^\n]+)$"
+code_regex = r"^```\s*?(\S[\s\S]*?)```$"
+unordered_list_regex = r"^[-*] (.*)"
+quote_regex = r"^>\s?(.*)"
 
 class BlockType(Enum):
     PARAGRAPH = "paragraph"
@@ -15,17 +19,14 @@ def markdown_to_blocks(markdown: str) -> list[str]:
     return [block.strip() for block in markdown.split("\n\n") if len(block) >= 1]
 
 
-heading_regex = r"^#{1,6}\s"
-code_regex = r"^```[\s\S]*?\S[\s\S]*?```$"
-unordered_list_regex = r"^[-*]\s+"
 
 
 def block_to_block_type(block: str) -> BlockType:
-    if re.match(heading_regex, block) and len(block.splitlines()) == 1:
+    if re.match(heading_regex, block):
         return BlockType.HEADING
     if re.match(code_regex, block):
         return BlockType.CODE
-    if all(line.startswith(">") for line in block.splitlines()):
+    if all(re.match(quote_regex, line) for line in block.splitlines()):
         return BlockType.QUOTE
     if all(re.match(unordered_list_regex, line) for line in block.splitlines()):
         return BlockType.UNORDERED_LIST
